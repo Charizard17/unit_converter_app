07c1be1d395e3712d1fb5b63dbc83947b011c28d
Charizard17 <3723esad@gmail.com>
2020-05-07 12:30:07 +0200

    unit change funcs added

--- measure.js
+++ measure.js
@@ -13,3 +13,91 @@ document.getElementById("mInput").addEventListener("input", function (e) {
 
   document.getElementById("yardOutput").innerHTML = m * 1.09361;
 });
+
+function changeCentimeter() {
+  document.getElementById("mInput").placeholder = "Enter Centimeter";
+
+  document.getElementById("mInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let cm = e.target.value;
+
+    document.getElementById("cmh4").innerText = "Meter";
+    document.getElementById("cmOutput").innerHTML = cm / 1000;
+
+    document.getElementById("feeth4").innerText = "Feet";
+    document.getElementById("feetOutput").innerHTML = cm * 3.28084 / 1000;
+    
+    document.getElementById("inchh4").innerText = "Inch";
+    document.getElementById("inchOutput").innerHTML = cm * 39.3701 / 1000;
+
+    document.getElementById("yardh4").innerText = "Yard";
+    document.getElementById("yardOutput").innerHTML = cm * 1.09361 / 1000;
+  });
+}
+
+function changeFeet() {
+  document.getElementById("mInput").placeholder = "Enter Feet";
+
+  document.getElementById("mInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let feet = e.target.value;
+
+    document.getElementById("cmh4").innerText = "Centimeter";
+    document.getElementById("cmOutput").innerHTML = feet * 30.48;
+
+    document.getElementById("feeth4").innerText = "Meter";
+    document.getElementById("feetOutput").innerHTML = feet * 0.3048;
+    
+    document.getElementById("inchh4").innerText = "Inch";
+    document.getElementById("inchOutput").innerHTML = feet * 12;
+
+    document.getElementById("yardh4").innerText = "Yard";
+    document.getElementById("yardOutput").innerHTML = feet * 0.333333;
+  });
+}
+
+function changeInch() {
+  document.getElementById("mInput").placeholder = "Enter Inch";
+
+  document.getElementById("mInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let inch = e.target.value;
+
+    document.getElementById("cmh4").innerText = "Centimeter";
+    document.getElementById("cmOutput").innerHTML = inch * 2.54;
+
+    document.getElementById("feeth4").innerText = "Feet";
+    document.getElementById("feetOutput").innerHTML = inch * 0.0833333;
+    
+    document.getElementById("inchh4").innerText = "Meter";
+    document.getElementById("inchOutput").innerHTML = inch * 0.0254;
+
+    document.getElementById("yardh4").innerText = "Yard";
+    document.getElementById("yardOutput").innerHTML = inch * 0.027778;
+  });
+}
+
+function changeYard() {
+  document.getElementById("mInput").placeholder = "Enter Yard";
+
+  document.getElementById("mInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let yard = e.target.value;
+
+    document.getElementById("cmh4").innerText = "Centimeter";
+    document.getElementById("cmOutput").innerHTML = yard * 91.44;
+
+    document.getElementById("feeth4").innerText = "Feet";
+    document.getElementById("feetOutput").innerHTML = yard * 3;
+    
+    document.getElementById("inchh4").innerText = "Inch";
+    document.getElementById("inchOutput").innerHTML = yard * 36;
+
+    document.getElementById("yardh4").innerText = "Meter";
+    document.getElementById("yardOutput").innerHTML = yard * 0.9144;
+  });
+}
--- weight.js
+++ weight.js
@@ -11,3 +11,60 @@ document.getElementById("kgInput").addEventListener("input", function (e) {
 
   document.getElementById("ozOutput").innerHTML = kg * 35274;
 });
+
+function changeGrams() {
+  document.getElementById("kgInput").placeholder = "Enter Grams";
+
+  document.getElementById("kgInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let gram = e.target.value;
+
+    document.getElementById("gramsh4").innerText = "Kilograms";
+    document.getElementById("gramsOutput").innerHTML = gram / 1000;
+
+    document.getElementById("poundsh4").innerText = "Pounds";
+    document.getElementById("poundsOutput").innerHTML = (gram * 2.2046) / 1000;
+
+    document.getElementById("ouncesh4").innerText = "Ounces";
+    document.getElementById("ozOutput").innerHTML = (gram * 35274) / 1000;
+  });
+}
+
+function changePounds() {
+  document.getElementById("kgInput").placeholder = "Enter Pounds";
+
+  document.getElementById("kgInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let pound = e.target.value;
+    
+    document.getElementById("gramsh4").innerText = "Grams";
+    document.getElementById("gramsOutput").innerHTML = pound * 453.592;
+
+    document.getElementById("poundsh4").innerText = "Kilograms";
+    document.getElementById("poundsOutput").innerHTML = pound * 0.453592;
+
+    document.getElementById("ouncesh4").innerText = "Ounces";
+    document.getElementById("ozOutput").innerHTML = pound * 16;
+  });
+}
+
+function changeOunces() {
+  document.getElementById("kgInput").placeholder = "Enter Ounces";
+
+  document.getElementById("kgInput").addEventListener("input", function (e) {
+    document.getElementById("output").style.visibility = "visible";
+
+    let ounce = e.target.value;
+    
+    document.getElementById("gramsh4").innerText = "Grams";
+    document.getElementById("gramsOutput").innerHTML = ounce * 28.3495;
+
+    document.getElementById("poundsh4").innerText = "Kilograms";
+    document.getElementById("poundsOutput").innerHTML = ounce * 0.0283495;
+
+    document.getElementById("ouncesh4").innerText = "Pounds";
+    document.getElementById("ozOutput").innerHTML = ounce * 0.0625;
+  });
+}
